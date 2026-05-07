c81eb7304f7b3f2629e54549b3a87c90d13cc99c
NahinKh <nahinkhan77@gmail.com>
2026-05-07 10:06:14 +0600

    Solution of generic function with constraints problem no.4

--- solutions.ts
+++ solutions.ts
@@ -1,4 +1,4 @@
-// Solutions of problems 1:
+// Solution of problems 1:
 const filterEvenNumbers = (numbers: number[]): number[] => {
     return numbers.filter(num => num % 2 === 0);
 }
@@ -6,14 +6,14 @@ const filterEvenNumbers = (numbers: number[]): number[] => {
 const evenNumbers = filterEvenNumbers([1, 2, 3, 4, 5, 6]);
 
 
-// Solutions of problems 2:
+// Solution of problems 2:
 const reverseString = (str: string): string => {
     return str.split('').reverse().join('');
 }
 const reversed = reverseString("typescript");
 
 
-// Solutions of problems 3:
+// Solution of problems 3:
 type StringOrNumber = string | number;
 const checkType = (val:StringOrNumber):StringOrNumber => {
     if (typeof val === 'string') {
@@ -22,3 +22,15 @@ const checkType = (val:StringOrNumber):StringOrNumber => {
         return "Number";
     }
 }
+
+// Solution of problems 4:
+type User = {
+    id: number;
+    name: string;
+    age: number;
+}
+const getProperty = <T extends User>(user: T, key: keyof User) => {
+    return user[key].toString();
+}
+const user = { id: 1, name: "John Doe", age: 21 };
+const userName = getProperty(user, "name");
